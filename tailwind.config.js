f79795de099604be1ecc056a218b810b408a2a6f
Jose Beltre <552006jose@gmail.com>
2024-07-09 11:45:00 -0400

    added most of the principal styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,38 @@ module.exports = {
         secondary: "#f1c40f",
         tertiary: "#e74c3c",
         quaternary: "#9b59b6",
-      }
+        background:{
+          light : "#E8E8E8",
+          dark : "#1D1D1D",
+        },
+        accent:{
+          light : "#FF7A00",
+          dark : "#FF7A00",
+        },
+        tile:{
+          light : "#e8e8e8",
+          dark : "#272727",
+        },
+        key:{
+          light : "#FFF",
+          dark : "#424242",
+        },
+        keyLetter:{
+          light : "#7A7A7A",
+          dark : "#BABABA",
+        }
+        
+      },
+      boxShadow:{
+        tileLight : '0px 4px 6.5px 0px rgba(0,0,0,0.15) inset',
+        tileDark : '0px 4px 12.1px 0px rgba(0,0,0,0.25) inset',
+        key: '1px 2px 2px 0px rgba(0,0,0,0.25)',
+      },
+      screens: {
+        'xs': {'min': '300px'},
+      },
     },
-  },
+    },
   plugins: [],
 }
 
